f4eb5dd8da959d0971979150eb888ad3e2b8e001
Roger Ramos <roger.ramos@gdpteam.com>
2023-02-15 14:38:35 -0500

    config: set light asdefault theme

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PRODUCTION = false
 export const DEBUG = false
 export const DEBUG_DELAY_MS = 800
-export const DEFAULT_THEME = 'dark'
+export const DEFAULT_THEME = 'light'
 export const API_SHOES = 'https://my-json-server.typicode.com/claumartinezh/training-db'
 export const APPSTATE_KEY = "shoes_app"
 export const STATE_PREFIX = "__SHOESAPP"
